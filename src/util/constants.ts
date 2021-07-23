25674fec440a3d217551adaf9852994084026644
lleyton <lleyton@innatical.com>
2021-07-23 15:24:58 -0700

    Update constants.ts

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -2,4 +2,4 @@ import fs from 'fs'
 
 export const jwtKey = process.env.JWT_KEY
 
-export const federationKey = process.env.FEDERATION_KEY_PUB!
+export const federationKey = process.env.JWT_FEDERATION_KEY_PUB!
